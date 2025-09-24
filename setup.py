d0eea414084ef96fdccbf539728093857b529115
amansidhu21 <sidhua11@mcmaster.ca>
2025-09-24 13:15:33 -0400

    fix: update latest src

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="emojify",
+    name="emojify-asid",
     version="0.1.0",
     description="A smart text emojifier",
     author="Your Name",
     packages=find_packages(),
     install_requires=[
-        "spacy>=3.0.0",
+        # "spacy>=3.0.0",
     ],
     entry_points={
         "console_scripts": [
